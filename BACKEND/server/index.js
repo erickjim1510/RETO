a6311250ca9136a7666f0ed9b00e838d06edf856
erickjim1510 <erickjimenez15803@gmail.com>
2026-05-22 10:48:03 -0600

    3ro

--- BACKEND/server/index.js
+++ BACKEND/server/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import { PORT } from './config.js';
 import clasificacionRoutes from './routes/clasificacion.routes.js';
-
+import usuarioRoutes from './routes/usuarios.routes.js';
+import generosRoutes from './routes/generos.routes.js';
+import peliculasRoutes from './routes/peliculas.routes.js'
+import salasRoutes from './routes/salas.routes.js'
+import tipoSalaRoutes from './routes/tipoSala.routes.js'
+import proyeccionesRoutes from './routes/proyeccion.routes.js'
+import cineRoutes from "./routes/cine.routes.js"
+import boletoRoutes from "./routes/boleto.routes.js"
+import pagoRoutes from "./routes/pago.routes.js"
 import cors from 'cors'; // Importar CORS
 
 const app = express();
@@ -11,7 +19,15 @@ app.use(express.json());
 
 app.use(cors());
 app.use(clasificacionRoutes);
-
+app.use(usuarioRoutes);
+app.use(generosRoutes);
+app.use(peliculasRoutes);
+app.use(salasRoutes)
+app.use(tipoSalaRoutes)
+app.use(proyeccionesRoutes)
+app.use(cineRoutes)
+app.use(boletoRoutes)
+app.use(pagoRoutes)
 
 
 app.listen(PORT); //abre servidor en puerto 4000
