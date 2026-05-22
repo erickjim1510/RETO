90bbda30ffb52b1f111da502e980b861c71cb664
Ian <iangoncen0@gmail.com>
2026-05-22 13:44:20 -0600

    pantalla kisko y camara

--- FRONTEND/CLIENTE/src/App.jsx
+++ FRONTEND/CLIENTE/src/App.jsx
@@ -11,25 +11,17 @@ import Clasificacion from "./pages/Clasificacion";
 import Salas from "./pages/Salas";
 import TipoSala from "./pages/TipoSala";
 import Proyecciones from "./pages/Proyecciones";
+import Kiosko from "./pages/Kiosko";
 import { AuthContextProvider } from "../src/context/AuthContext";
 
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
-        {/* Subrutas dentro de Home */}
-        <Route index element={<h2>Bienvenido al Sistema</h2>} />
-        <Route path="genero" element={<Genero />} />
-        <Route path="clasificacion" element={<Clasificacion />} />
-        <Route path="pelicula" element={<Pelicula />} />
-        <Route path="salas" element={<Salas />} />
-        <Route path="tipoSala" element={<TipoSala />} />
-        <Route path="proyecciones" element={<Proyecciones />} />
-      </Route>
       <Route path="/" element={<Login />} />
       <Route path="/cartelera" element={<Cartelera />} />
       <Route path="/Registro" element={<Registro />} />
       <Route path="/Sala" element={<Sala />} />
+      <Route path="/kiosko" element={<Kiosko />} />
       <Route path="*" element={<Notfound />} />
     </Routes>
   );
